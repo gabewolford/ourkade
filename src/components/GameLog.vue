1201a9dc3787fb54f697df2847ac60f4635f5e5a
gabewolford <gabewolford@gmail.com>
2023-11-02 15:40:41 -0700

    created game log component

---
+++ src/components/GameLog.vue
@@ -0,0 +1,58 @@
+<template>
+  <div
+    class="flex flex-col gap-6 w-2/3 bg-[#0F0F0F]/60 p-4 mb-6 rounded-[20px]"
+  >
+    <div class="flex flex-row gap-4 items-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="38"
+        height="48"
+        viewBox="0 0 38 48"
+        fill="none"
+      >
+        <g style="mix-blend-mode: overlay">
+          <path
+            d="M22.2161 15.3256V4.44938H27.2782V15.3256H29.8092V17.5503H32.059V15.3256H34.59V13.1009H29.8092V2.22469H27.2782V0H22.2161V2.22469H19.9663V13.1009H14.9043V15.3256H17.4353V17.5503H19.6851V15.3256H22.2161Z"
+            fill="white"
+          />
+          <path
+            d="M19.6855 17.2979H22.2166V19.5225H19.6855V17.2979Z"
+            fill="white"
+          />
+          <path
+            d="M22.2188 19.5283V21.753H27.2808V19.5283H22.2188Z"
+            fill="white"
+          />
+          <path
+            d="M14.908 28.4291H17.439V23.9797H14.908V15.3281H12.377V26.2044H14.908V28.4291Z"
+            fill="white"
+          />
+          <path
+            d="M17.4355 21.753V23.9777H19.9666V19.5283H17.4355V21.753Z"
+            fill="white"
+          />
+          <path
+            d="M34.5935 15.3281V23.9797H32.0625V28.4291H34.5935V26.2044H37.1245V15.3281H34.5935Z"
+            fill="white"
+          />
+          <path
+            d="M27.2812 17.2979H29.8123V19.5225H27.2812V17.2979Z"
+            fill="white"
+          />
+          <path
+            d="M27.2788 26.1597V45.732H22.2168V23.9795H19.967V39.3051H17.4359V37.0804H9.84287V34.8557H0V37.0804H2.53102V39.3051H7.59307V41.2826H12.3739V43.5073H17.1547V45.732H22.2168V47.9567H29.8098V23.9795H27.2788V26.1597Z"
+            fill="white"
+          />
+          <path
+            d="M29.8105 23.9777H32.3416V19.5283H29.8105V23.9777Z"
+            fill="white"
+          />
+        </g>
+      </svg>
+      <div class="flex flex-col text-start">
+        <h5 class="mix-blend-overlay text-lg">kade runner</h5>
+        <h4 class="text-[40px] leading-8">Game Log</h4>
+      </div>
+    </div>
+  </div>
+</template>
