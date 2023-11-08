a5ce705ba831ba31a32026597cd7dc6717044acb
gabewolford <gabewolford@gmail.com>
2023-11-08 12:05:51 -0800

    updated tv screen inset shadow

--- src/components/MenuScreen.vue
+++ src/components/MenuScreen.vue
@@ -22,7 +22,7 @@ export default {
 <template>
   <div
     v-show="currentSlide === 1"
-    class="flex flex-row gradient1 w-full h-full rounded-[30px] carousel-slide tv-shadow"
+    class="flex flex-row gradient1 w-full h-full rounded-[30px] carousel-slide shadow-tvShadow"
     id="slide1"
   >
     <button
@@ -47,7 +47,7 @@ export default {
 
   <div
     v-show="currentSlide === 2"
-    class="flex flex-row gradient2 w-full h-full rounded-[30px] relative carousel-slide tv-shadow"
+    class="flex flex-row gradient2 w-full h-full rounded-[30px] relative carousel-slide shadow-tvShadow"
     id="slide2"
   >
     <div class="absolute top-8 right-11 lg:top-24 lg:right-32">
@@ -95,7 +95,7 @@ export default {
 
   <div
     v-show="currentSlide === 3"
-    class="flex flex-row gradient3 w-full h-full rounded-[30px] carousel-slide tv-shadow"
+    class="flex flex-row gradient3 w-full h-full rounded-[30px] carousel-slide shadow-tvShadow"
     id="slide3"
   >
     <button @click="prevSlide" class="flex justify-center items-center w-2/12">
@@ -145,9 +145,5 @@ export default {
 }
 .gradient3 {
   background: linear-gradient(270deg, #871b89 0.02%, #1f4ec6 100%);
-}
-
-.tv-shadow {
-  box-shadow: 0px 0px 50px 25px #656565 inset;
 }
 </style>
