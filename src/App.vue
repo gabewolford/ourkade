9af510a668bf1fb8f02ca795eab451e83cbb1e33
gabewolford <gabewolford@gmail.com>
2023-11-08 12:20:47 -0800

    set max width

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,9 @@ import AppHeader from "@/components/AppHeader.vue";
 <template>
   <div class="px-4 lg:px-6 2xl:px-12">
     <AppHeader />
-    <div class="flex flex-row gap-4 lg:gap-6 pb-[200px]">
+    <div
+      class="flex flex-row gap-4 lg:gap-6 pb-[200px] 2xl:max-w-[80vw] 2xl:mx-auto"
+    >
       <AppNav />
       <div class="flex flex-col w-full mb-4 lg:mb-6">
         <router-view />
