ec60f1f19d2ec033a6ab39471b5c692a9b362d8f
gabewolford <gabewolford@gmail.com>
2023-11-11 11:38:58 -0800

    challenge table done

--- src/components/ChallengeScreen.vue
+++ src/components/ChallengeScreen.vue
@@ -29,7 +29,7 @@
       </div>
       <div class="flex flex-col md:w-5/12 gap-2">
         <h2 class="hidden md:flex uppercase text-sm md:text-xl xl:text-2xl">
-          Competition Mode
+          Challenge Mode
         </h2>
         <button
           class="flex flex-row items-center max-w-[250px] md:max-w-full md:w-full relative"
@@ -45,15 +45,37 @@
         <h2 class="uppercase text-sm md:text-xl xl:text-2xl pt-4">
           Sample Challenge
         </h2>
-        <p class="text-xs md:text-base lg:text-lg xl:text-xl text-[#46FFBC]">
-          best: <span class="text-white">1800pts</span>
-        </p>
-        <p class="text-xs md:text-base lg:text-lg xl:text-xl text-[#46FFBC]">
-          last: <span class="text-white">1231pts</span>
-        </p>
-        <p class="text-xs md:text-base lg:text-lg xl:text-xl text-[#46FFBC]">
-          avg: <span class="text-white">1231pts</span>
-        </p>
+        <div class="flex flex-col gap2">
+          <table class="flex items-center border border-[#7988AF] w-fit">
+            <td class="border-r border-[#7988AF] px-2.5">
+              <img src="../assets/table-icons/gray/tix.svg" alt="tix" />
+            </td>
+            <td class="border-r border-[#7988AF] px-2.5 pt-1.5">1</td>
+            <td class="border-r border-[#7988AF] px-2.5">
+              <img
+                src="../assets/table-icons/gray/players.svg"
+                alt="players"
+                class="py-1"
+              />
+            </td>
+            <td class="border-r border-[#7988AF] px-2.5 pt-1.5">3</td>
+            <td class="border-r border-[#7988AF] px-2.5">
+              <img src="../assets/table-icons/gray/time.svg" alt="time" />
+            </td>
+            <td class="border-r border-[#7988AF] px-2.5 pt-1.5">24:00</td>
+            <td class="border-r border-[#7988AF] px-2.5">
+              <img src="../assets/table-icons/gray/trophy.svg" alt="trophy" />
+            </td>
+            <td class="px-2.5 pt-0.5 flex flex-row items-center h-full gap-2">
+              <p class="text-[#E3BA24] pt-1">2</p>
+              <img
+                src="../assets/table-icons/gray/gold-ticket.svg"
+                alt="tix"
+                class="pr-2.5"
+              />
+            </td>
+          </table>
+        </div>
       </div>
     </div>
   </div>
