12dd929ce929ef1ae314854bd01f285bef4af0cd
gabewolford <gabewolford@gmail.com>
2023-11-21 14:05:10 -0800

    changed button text, moved home to first buttons

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -9,6 +9,19 @@ const $route = useRoute();
     class="grid grid-cols-2 lg:grid-cols-4 justify-between gap-2 lg:gap-6 text-lg lg:text-xl xl:text-3xl 2xl:text-4xl"
     :class="{}"
   >
+    <router-link :to="{ name: 'Home' }">
+      <button
+        class="flex flew-row flex-1 gap-2 items-center rounded-full 2xl:rounded-[50px] bg-[#783B28] hover:bg-[#794332] w-full min-w-fit p-2 lg:p-4 h-fit lg:h-[75px] xl:h-[100px] 2xl:h-[125px] font-nano-pix"
+        :class="{
+          'shadow-custom2': $route.name === 'Home',
+          'shadow-custom4': $route.name !== 'Home',
+        }"
+      >
+        <img src="../assets/button-icons/menu-icon.svg" alt="menu icon" />
+        <h3>Home</h3>
+      </button>
+    </router-link>
+
     <router-link :to="{ name: 'Compete1' }">
       <button
         class="flex flew-row flex-1 gap-2 items-center rounded-full 2xl:rounded-[50px] bg-[#3d4D7C] hover:bg-[#425A9F] w-full min-w-fit p-2 lg:p-4 h-fit lg:h-[75px] xl:h-[100px] 2xl:h-[125px] font-nano-pix"
@@ -51,19 +64,6 @@ const $route = useRoute();
           alt="practice icon"
         />
         <h3>Practice</h3>
-      </button>
-    </router-link>
-
-    <router-link :to="{ name: 'Home' }">
-      <button
-        class="flex flew-row flex-1 gap-2 items-center rounded-full 2xl:rounded-[50px] bg-[#783B28] hover:bg-[#794332] w-full min-w-fit p-2 lg:p-4 h-fit lg:h-[75px] xl:h-[100px] 2xl:h-[125px] font-nano-pix"
-        :class="{
-          'shadow-custom2': $route.name === 'Home',
-          'shadow-custom4': $route.name !== 'Home',
-        }"
-      >
-        <img src="../assets/button-icons/menu-icon.svg" alt="menu icon" />
-        <h3>Menu</h3>
       </button>
     </router-link>
   </div>
