1e036f651cfcb268dd993d66f63aaeb8b0156686
gabewolford <gabewolford@gmail.com>
2023-11-21 15:01:37 -0800

    cleaning up

--- src/components/PracticeScreen.vue
+++ src/components/PracticeScreen.vue
@@ -1,33 +1,6 @@
-<style scoped>
-.background-image {
-  background-image: url("../assets/kaderunner-bg.png");
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-}
-</style>
-
 <script setup>
-import { ref, computed } from "vue";
-import Practice1 from "@/components/PracticeScreens/Practice1.vue";
 import ScreenLayout from "@/components/ScreenLayout.vue";
 import competeLogo from "@/assets/kaderunner-logo-green.png";
-
-// const activeScreen = ref(1);
-
-// const getActiveScreenComponent = computed(() => {
-//   switch (activeScreen.value) {
-//     case 1:
-//       return Practice1;
-//     default:
-//       return Compete1;
-//   }
-// });
-
-// const switchToScreen = (screenNumber) => {
-//   activeScreen.value = screenNumber;
-// };
 </script>
 
 <template>
@@ -74,3 +47,13 @@ import competeLogo from "@/assets/kaderunner-logo-green.png";
     </div>
   </ScreenLayout>
 </template>
+
+<style scoped>
+.background-image {
+  background-image: url("../assets/kaderunner-bg.png");
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
