34b14406be5ed9ab712ace1bb4e6578c6367a118
gabewolford <gabewolford@gmail.com>
2023-11-21 19:34:55 -0800

    email confirmation page complete

--- src/pages/EmailConfirmation.vue
+++ src/pages/EmailConfirmation.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>
-    <h1 class="text-3xl">Thanks for registering!</h1>
-    <p>
-      Please confirm your email to finishing registering:
-      {{ $route.query.email }}
-    </p>
+  <div
+    class="flex flex-col text-center justify-center items-center font-nano-pix my-6 lg:my-24 w-2/3 mx-auto gap-6"
+  >
+    <img
+      src="../assets/email-icon.svg"
+      alt="email icon"
+      class="w-1/4 lg:w-1/5"
+    />
+    <h4 class="text-3xl">
+      We emailed a link to
+      <span class="text-[#46FFBC]">{{ $route.query.email }}</span> in order to
+      verify this address.
+    </h4>
+    <h4 class="text-3xl">Please click on it to complete sign up.</h4>
   </div>
 </template>
