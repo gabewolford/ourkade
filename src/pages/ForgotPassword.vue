300efd0a61e2a37bb3750ec7d990cede34ffcd19
gabewolford <gabewolford@gmail.com>
2023-11-30 13:53:39 -0800

    created forgot password page

--- src/pages/ForgotPassword.vue
+++ src/pages/ForgotPassword.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from "vue";
-import { useAuthStore } from '@/stores/auth'
+import { useAuthStore } from "@/stores/auth";
 
 const authStore = useAuthStore();
 
@@ -15,9 +15,49 @@ const handlePasswordReset = async () => {
 };
 </script>
 <template>
-  <form class="max-w-lg m-auto" @submit.prevent="auth.handlePasswordReset()">
-    <h1 class="text-3xl mb-5">Forgot Password?</h1>
-    <label>Email <input v-model="email" type="email" /></label>
-    <button>Send Reset Email</button>
-  </form>
+  <section
+    class="flex flex-col w-full bg-formDarkPurple/60 rounded-[20px] lg:h-[450px] p-4 lg:p-8 text-white gap-6"
+  >
+    <div
+      class="flex flex-col lg:flex-row lg:items-center gap-4 lg:gap-12 font-nano-pix"
+    >
+      <div class="flex flex-row gap-2">
+        <img src="../assets/icons/account-icon.svg" alt="account icon" />
+        <h1 class="text-3xl">Account</h1>
+      </div>
+
+      <div class="flex flex-row text-xl">
+        <router-link
+          to="/register"
+          class="border-2 border-lightPurple px-10 py-1 rounded-l-xl text-[#757575]"
+        >
+          Sign Up
+        </router-link>
+        <p class="bg-lightPurple px-10 py-1 rounded-r-xl">Log In</p>
+      </div>
+    </div>
+
+    <form
+      @submit.prevent="auth.handlePasswordReset()"
+      class="flex flex-col m-auto w-full lg:w-1/2 gap-4"
+    >
+      <div class="flex flex-col">
+        <label for="email" class="pl-2">Email</label>
+        <input
+          v-model="email"
+          type="email"
+          id="email"
+          class="!bg-lightPurple px-6 py-4 lg:py-5 lg:text-lg rounded-2xl text-[#603961]"
+        />
+      </div>
+
+      <div class="flex flex-row items-center justify-between mt-2">
+        <button
+          class="rainbow hover:shadow-customWhite transition duration-300 font-nano-pix text-black w-fit px-8 py-2 text-xl rounded-xl"
+        >
+          Send Reset Email
+        </button>
+      </div>
+    </form>
+  </section>
 </template>
